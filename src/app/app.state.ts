c373ebd2e7524e9dbfda6a5b1b1e2c14bfecaae0
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-08-12 00:35:47 +0100

    comment

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -3,3 +3,5 @@ import { Tutorial } from '../app/models/models.tutorial'
 export interface AppState {
     readonly tutorial: Tutorial[]
 }
+// We will import this file within the components that we wish to access ngrx.
+
