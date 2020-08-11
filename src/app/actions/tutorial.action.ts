86e2f4a97d897c301355c472b42bebeac06ac305
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-08-11 23:56:41 +0100

    adding actions

--- src/app/actions/tutorial.action.ts
+++ src/app/actions/tutorial.action.ts
@@ -1,3 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Tutorial } from '../../app/models/models.tutorial'
+
+export const ADD_TUTORIAL = '[TUTORIAL]add'
+export const REMOVE_TUTORIAL = '[TUTORIAL]remove'
