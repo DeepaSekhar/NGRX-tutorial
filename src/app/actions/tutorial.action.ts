5a70b5fd7b2674829b1d19b37973cb32ba0c7ec6
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-08-12 00:03:36 +0100

    creating a class for each action that allows the payload to pass in

--- src/app/actions/tutorial.action.ts
+++ src/app/actions/tutorial.action.ts
@@ -4,3 +4,12 @@ import { Tutorial } from '../../app/models/models.tutorial'
 
 export const ADD_TUTORIAL = '[TUTORIAL]add'
 export const REMOVE_TUTORIAL = '[TUTORIAL]remove'
+
+export class addTutorial implements Action {
+    readonly type = ADD_TUTORIAL
+    constructor(public payload: Tutorial) { }
+}
+export class removeTutorial implements Action {
+    readonly type = REMOVE_TUTORIAL
+    constructor(public payload: Tutorial) { }
+}
