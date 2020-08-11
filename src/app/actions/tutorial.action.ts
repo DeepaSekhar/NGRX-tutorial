f380bdcf30200c282a1195b0a01993a1f1a6e964
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-08-12 00:09:39 +0100

    giving comments on actions

--- src/app/actions/tutorial.action.ts
+++ src/app/actions/tutorial.action.ts
@@ -1,9 +1,15 @@
+//  importing our Tutorial model and Action from ngrx/store.
+
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Tutorial } from '../../app/models/models.tutorial'
 
+// defining the type of action, which is in the form of a string constant.
+
 export const ADD_TUTORIAL = '[TUTORIAL]add'
 export const REMOVE_TUTORIAL = '[TUTORIAL]remove'
+
+//  creating a class for each action with a constructor that allows us to pass in the payload. 
 
 export class AddTutorial implements Action {
     readonly type = ADD_TUTORIAL
