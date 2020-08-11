23eb205427727535ae755158fdab7c77ee82857c
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-08-12 00:06:14 +0100

    exporting all of our action classes for use within our upcoming reducer.

--- src/app/actions/tutorial.action.ts
+++ src/app/actions/tutorial.action.ts
@@ -5,11 +5,14 @@ import { Tutorial } from '../../app/models/models.tutorial'
 export const ADD_TUTORIAL = '[TUTORIAL]add'
 export const REMOVE_TUTORIAL = '[TUTORIAL]remove'
 
-export class addTutorial implements Action {
+export class AddTutorial implements Action {
     readonly type = ADD_TUTORIAL
     constructor(public payload: Tutorial) { }
 }
-export class removeTutorial implements Action {
+export class RemoveTutorial implements Action {
     readonly type = REMOVE_TUTORIAL
     constructor(public payload: Tutorial) { }
 }
+// exporting all of our action classes for use within our upcoming reducer.
+
+export type Actions = AddTutorial | RemoveTutorial
